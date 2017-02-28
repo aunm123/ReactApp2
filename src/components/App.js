40d4660d305763d39914987fedef1374a862c954
Tim Huo <aunm123@yeah.net>
2017-02-28 18:25:46 +0800

    Init project

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+var imageDatas = require('../data/imageDatas.json');
+
+imageDatas = ((imageDataArr) => {
+	
+	for (let i=0;i<imageDataArr.length;i++){
+		let singleImageData = imageDataArr[i];
+		
+		singleImageData["imageURL"] = require("../../public/images/"+singleImageData["fileName"]);
+		
+		imageDataArr[i] = singleImageData;
+	}
+	return imageDataArr;
+})(imageDatas);
+
+console.log(imageDatas);
+
+class App extends Component {
+  render() {
+    
+    var controllerUnits = [],
+        imgFigures =[];
+    
+    imageDatas.forEach((value)=>{
+			imgFigures.push((<ImgFigure dataSource={value}/>))
+			
+			alert(value);
+    });
+    
+    return (
+      <section className="stage">
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+    );
+  }
+}
+
+class ImgFigure extends Component {
+  render() {
+    return (
+      <figure>
+        <img src={this.props.data["imageURL"]}
+              alt={this.props.data["title"]}/>
+        <figcaption>
+          <h2>{this.props.data["title"]}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+
+export default App;
